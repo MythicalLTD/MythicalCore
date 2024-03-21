0497514792fd4dbb51e968d1e5d5b58b21e7a268
NaysKutzu <87282334+NaysKutzu@users.noreply.github.com>
2024-03-21 12:11:52 +0100

    PUSH -> Added more options for the api handler

--- src/MythicalSystems/Api/ResponseHandler.php
+++ src/MythicalSystems/Api/ResponseHandler.php
@@ -14,7 +14,7 @@ class ResponseHandler
      */
     public static function OK(string|null $message): void
     {
-        self::sendManualResponse(200, null,$message);
+        self::sendManualResponse(200, null, $message, true,null);
     }
 
     /**
@@ -26,7 +26,7 @@ class ResponseHandler
      */
     public static function Created(string|null $message): void
     {
-        self::sendManualResponse(201, "The request has been fulfilled and a new resource has been created.", $message);
+        self::sendManualResponse(201, "The request has been fulfilled and a new resource has been created.", $message, true,null);
     }
 
     /**
@@ -38,7 +38,7 @@ class ResponseHandler
      */
     public static function NoContent(string|null $message): void
     {
-        self::sendManualResponse(204, "The server has successfully fulfilled the request and there is no content to send in the response.",$message);
+        self::sendManualResponse(204, "The server has successfully fulfilled the request and there is no content to send in the response.", $message, null,null);
     }
 
     /**
@@ -50,7 +50,7 @@ class ResponseHandler
      */
     public static function BadRequest(string|null $message): void
     {
-        self::sendManualResponse(400, "The server cannot process the request due to a client error (e.g., malformed syntax).",$message);
+        self::sendManualResponse(400, "The server cannot process the request due to a client error (e.g., malformed syntax).", $message, false,null);
     }
 
     /**
@@ -62,7 +62,7 @@ class ResponseHandler
      */
     public static function Unauthorized(string|null $message): void
     {
-        self::sendManualResponse(401, "The client must authenticate itself to get the requested response.",$message);
+        self::sendManualResponse(401, "The client must authenticate itself to get the requested response.", $message, false, null );
     }
 
     /**
@@ -74,7 +74,7 @@ class ResponseHandler
      */
     public static function Forbidden(string|null $message): void
     {
-        self::sendManualResponse(403, "The server understood the request, but refuses to authorize it.",$message);
+        self::sendManualResponse(403, "The server understood the request, but refuses to authorize it.", $message, false, null);
     }
 
     /**
@@ -86,7 +86,7 @@ class ResponseHandler
      */
     public static function NotFound(string|null $message): void
     {
-        self::sendManualResponse(404, "The requested resource could not be found on the server.",$message);
+        self::sendManualResponse(404, "The requested resource could not be found on the server.", $message, false, null);
     }
 
     /**
@@ -98,7 +98,7 @@ class ResponseHandler
      */
     public static function MethodNotAllowed(string|null $message): void
     {
-        self::sendManualResponse(405, "The method specified in the request is not allowed for the resource identified by the request URI.",$message);
+        self::sendManualResponse(405, "The method specified in the request is not allowed for the resource identified by the request URI.", $message, false, null);
     }
 
     /**
@@ -110,7 +110,7 @@ class ResponseHandler
      */
     public static function InternalServerError(string|null $message): void
     {
-        self::sendManualResponse(500, "A generic error message, given when an unexpected condition was encountered and no more specific message is suitable.",$message);
+        self::sendManualResponse(500, "A generic error message, given when an unexpected condition was encountered and no more specific message is suitable.", $message, false, null);
     }
     /**
      * Return a 502 response
@@ -121,7 +121,7 @@ class ResponseHandler
      */
     public static function BadGateway(string|null $message): void
     {
-        self::sendManualResponse(502, "The server, while acting as a gateway or proxy, received an invalid response from the upstream server it accessed in attempting to fulfill the request.",$message);
+        self::sendManualResponse(502, "The server, while acting as a gateway or proxy, received an invalid response from the upstream server it accessed in attempting to fulfill the request.", $message, false, null);
     }
     /**
      * Return a 503 response
@@ -132,27 +132,32 @@ class ResponseHandler
      */
     public static function ServiceUnavailable(string|null $message): void
     {
-        self::sendManualResponse(503, "The server is currently unable to handle the request due to temporary overloading or maintenance of the server.",$message);
+        self::sendManualResponse(503, "The server is currently unable to handle the request due to temporary overloading or maintenance of the server.", $message, false, null);
     }
-    
+
     /**
      * Return a manual response code!
      * 
-     * @param int $code The http code
+     * @param int $code The HTTP status code
      * @param string|null $error If you want to show any errors
      * @param string|null $message Any message you want to tell the user?
+     * @param bool|null $success 1 for yes 0 for no
+     * @param array|null $extraContent Any extra content to include in the response
      * 
      * @return void
      */
-    public static function sendManualResponse(int $code, string|null $error, string|null $message): void
+    public static function sendManualResponse(int $code, string|null $error, string|null $message, bool|null $success, array|null $extraContent): void
     {
         $response = [
             "code" => $code,
             "error" => $error,
-            "message" => $message
+            "message" => $message,
+            "success" => $success,
+            "extra_content" => $extraContent
         ];
 
         http_response_code($code);
-        die(json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+        die (json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
     }
+
 }
